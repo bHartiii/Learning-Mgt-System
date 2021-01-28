b25ee6376745cd20af0e0f6ab72bc9c81f75e151
malibharti5@gmail.com <malibharti5@gmail.com>
2021-01-28 10:47:33 +0530

    Bharti-m : Swagger configuration is addded

--- LearningMgtSystem/settings.py
+++ LearningMgtSystem/settings.py
@@ -32,6 +32,7 @@ AUTH_USER_MODEL = 'authentication.User'
 
 INSTALLED_APPS = [
     'rest_framework',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
