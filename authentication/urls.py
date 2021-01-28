4dab5cbab99ae63c4ac8d236ae651ba1811c5d8b
malibharti5@gmail.com <malibharti5@gmail.com>
2021-01-28 10:46:20 +0530

    Bharti-ADD :  User creation view is created

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from authentication.views import UserCreationAPIView
 
 urlpatterns = [
-    
+    path('create-user/', UserCreationAPIView.as_view(), name='create-user'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from authentication.serializers import UserCreationSerializer
+from authentication.models import User
 
-# Create your views here.
+class UserCreationAPIView(generics.GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserCreationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        user = User.objects.create()
+        user.save()
+        return Response({'New user is created successfully!!!!!'})
