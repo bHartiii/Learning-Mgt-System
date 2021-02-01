d7706cd8a50a97c0a16c8a99b72924a575bde176
malibharti5@gmail.com <malibharti5@gmail.com>
2021-02-01 17:21:41 +0530

    Bharti-ADD : Test cases for create-user API

--- authentication/test/test_views.py
+++ authentication/test/test_views.py
@@ -12,7 +12,8 @@ class AuthenticationAPITest(TestCase):
     def setUp(self):
         # initialize the APIClient app
         self.client = Client()
-        self.admin = User.objects.create(username='admin', first_name='Bharti', last_name='Mali', role='Admin', email='admin@gmail.com', password='pbkdf2_sha256$216000$jge4gjoUWquH$RqjqlYMi9gKUAEItd91thLEwpewzUn5WOfL2TdsLyjY=')
+
+        self.admin = User.objects.create(username='admin', first_name='Bharti', last_name='Mali', role='Admin', email='admin@gmail.com', password='pbkdf2_sha256$216000$yG97oIyabA7B$nzM1GiofqbxOqH/prQTgjDWhT1C7o1cpj0VdJ5viW0M=', is_superuser=True)
         self.student = User.objects.create(username='student', first_name='Bharti', last_name='Mali', role='Student', email='student@gmail.com', password='pbkdf2_sha256$216000$jge4gjoUWquH$RqjqlYMi9gKUAEItd91thLEwpewzUn5WOfL2TdsLyjY=')
         self.mentor = User.objects.create(username='mentor', first_name='Bharti', last_name='Mali', role='Mentor', email='mentor@gmail.com', password='pbkdf2_sha256$216000$jge4gjoUWquH$RqjqlYMi9gKUAEItd91thLEwpewzUn5WOfL2TdsLyjY=')
 
@@ -35,11 +36,51 @@ class AuthenticationAPITest(TestCase):
             'mobile_number':'1234567890',
             'role' : 'Admin'
         }
+        self.admin_login_payload = {
+            'username' : 'admin',
+            'password' : 'bharti'
+        }
+        self.mentor_login_payload = {
+            'username' : 'mentor',
+            'password' : 'bharti'
+        }
+        self.student_login_payload = {
+            'username' : 'student',
+            'password' : 'bharti'
+        }
+        self.invalid_login_payload = {
+            'username' : 'bharti',
+            'password' : 'bharti'
+        }
 
-    def test_create_user_with_valid_payload(self):
-        response = self.client.post(reverse('create-user'),data=json.dumps(self.valid_user_payload) ,content_type=CONTENT_TYPE)
+### Test cases for create-user API : 
+
+    def test_create_user_with_valid_payload_without_login(self):
+        response = self.client.post(reverse('create-user'), data=json.dumps(self.valid_user_payload), content_type=CONTENT_TYPE)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_user_with_valid_payload_after_login_by_invalid_credentials(self):
+        self.client.post(reverse('login'), data=json.dumps(self.invalid_login_payload), content_type=CONTENT_TYPE)
+        response = self.client.post(reverse('create-user'), data=json.dumps(self.valid_user_payload), content_type=CONTENT_TYPE)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_user_with_valid_payload_after_login_by_admin_credentials(self):
+        self.client.post(reverse('login'), data=json.dumps(self.admin_login_payload), content_type=CONTENT_TYPE)
+        response = self.client.post(reverse('create-user'), data=json.dumps(self.valid_user_payload), content_type=CONTENT_TYPE)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_user_with_invalid_payload(self):
-        response = self.client.post(reverse('create-user'),data=json.dumps(self.invalid_user_payload) ,content_type=CONTENT_TYPE)
+        self.client.post(reverse('login'), data=json.dumps(self.admin_login_payload), content_type=CONTENT_TYPE)
+        response = self.client.post(reverse('create-user'), data=json.dumps(self.invalid_user_payload), content_type=CONTENT_TYPE)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_with_valid_payload_after_login_by_mentor_credentials(self):
+        self.client.post(reverse('login'), data=json.dumps(self.mentor_login_payload), content_type=CONTENT_TYPE)
+        response = self.client.post(reverse('create-user'), data=json.dumps(self.valid_user_payload), content_type=CONTENT_TYPE)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_create_user_with_valid_payload_after_login_by_studnet_credentials(self):
+        self.client.post(reverse('login'), data=json.dumps(self.student_login_payload), content_type=CONTENT_TYPE)
+        response = self.client.post(reverse('create-user'), data=json.dumps(self.valid_user_payload), content_type=CONTENT_TYPE)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
