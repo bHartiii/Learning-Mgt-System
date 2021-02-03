c0aa54f7bd3dee065ec8739fdded11065cfac150
malibharti5@gmail.com <malibharti5@gmail.com>
2021-02-03 23:29:46 +0530

    Bharti-FIX : Media url is added

--- LearningMgtSystem/urls.py
+++ LearningMgtSystem/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -39,4 +39,4 @@ urlpatterns = [
     path('auth/', include('authentication.urls')),
     path('lms/', include('learning_mgt.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
